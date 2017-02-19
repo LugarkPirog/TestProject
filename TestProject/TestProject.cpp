0f8fa5e15cfefdf3e1b68afce005c3fec7a10219
LugarkPirog <sabaka4ka@mail.ru>
2017-02-19 15:45:04 +0300

    d

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -6,23 +6,7 @@ using namespace std;
 
 double avg(int *a);
 string tmp;
-int main()
-{/*
-	int X[3] = { 60, 50, 75 };
-	int Y[3] = { 10, 7, 12 };
-	double beta0, beta1, sum1 = 0, sum2 = 0;
-	for (int i = 0; i < 3; i++) {
-		sum1 += (X[i] * (avg(Y) - Y[i]));
-		sum2 += (X[i] * (avg(X) - X[i]));
-	}
-	printf("%f\n%f\n", sum1, sum2);
-	printf("%f\n%f\n", avg(X), avg(Y));
-	beta1 = sum1 / sum2;
-	beta0 = avg(Y) - avg(X)*beta1;
-	printf("%f\n%f", beta0, beta1);
-	/*auto t = [](int x) -> double {return sin(x); }; 
-
-	printf("%d", t); */
+int main(){
 	cout << "Enter the command: ";
 	for (;;) {
 		if (getline(cin, tmp)) {
@@ -118,32 +102,8 @@ int main()
 				cout << "det - calculating the determinant" << endl;
 				cout << "rev - finding the reverse matrix" << endl;
 			}
-			else if (tmp == "lr") {
-				int n, m;
-				cout << "Enter the number of parametres: ";
-				cin >> n;
-				cout << "Enter the number of experiments: ";
-				cin >> m;
-				Matrix all(m, n);
-
-			}
 		};
 	}
-	
 	system("pause");
     return 0;
-}
-
-double avg(int a[]) {
-	double avg_val = 0;
-	int n = 0;
-	for (int i = 0; i < 3; i++) {
-		avg_val += a[i];
-	}
-	return avg_val / 3;
-};
-
-Matrix get_sum() {
-	
-	return 0;
 }
